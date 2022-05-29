c50e9b23eda0c746ef6fe8f692ddebb9a292a375
jcmunday <62025739+1602077@users.noreply.github.com>
2022-05-29 22:34:33 +0100

    feat: add python sever

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,16 +1,48 @@
 -- LSP Setup
+-- Servers:
+-- 1. go: `go install golang.org/x/tools/gopls@latest`
+-- 2. python: `pip3 install python-lsp-server`
+-- 3. kubernetes: default yaml
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    require('lspconfig')['gopls'].setup {
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    require("lspconfig")["gopls"].setup {
         capabilities = capabilities
 }
 
 vim.opt.completeopt={"menu", "menuone", "noselect"}
  
 
--- Golang: gopls
-require('lspconfig').gopls.setup{
-    on_attach = function()
+-- Autocompletions: nvim-cmp
+local cmp = require"cmp"
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    }, {
+        { name = "buffer" },
+    })
+})
+
+-- Set attach mappings
+local attach = function()
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
@@ -24,36 +56,23 @@ require('lspconfig').gopls.setup{
 
     vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
     vim.keymap.set("n", "R", vim.lsp.buf.references, {buffer=0})
-    end,
+end
+ 
+-- Golang: gopls
+require("lspconfig").gopls.setup{
+    on_attach = attach
 }
 
+-- Python: pylsp
+require("lspconfig").pylsp.setup{
+    on_attach = attach
+}
 
--- Autocompletions: nvim-cmp
-local cmp = require'cmp'
-
-cmp.setup({
-    snippet = {
-      expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ['<tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior }),
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
-    })
-})
-
+-- Kubernetes: yaml
+require("lspconfig").yamlls.setup{
+    settings = {
+        yaml = {
+            schemas = { kubernetes = "globPattern"},
+        }
+    }
+}
